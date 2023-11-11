4ff20497fecff62e1ce7e48ec16e34233ad9d43b
CassiaAlvalaz <cassia_alvalaz@hotmail.com>
2023-11-11 17:21:44 -0300

    Inclusão commit.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "99999-9999", modelo: "Modelo Nokia", imei: "teste", memoria: 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "11111-1111", modelo: "Modelo Iphone", imei: "testando", memoria: 64);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
